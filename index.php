368a3e7c25b2a9d4efd87dba40197e33f392912a
RAJBALI KUMAR <91846008+therajbali@users.noreply.github.com>
2023-08-29 16:42:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,4 @@
-<?php
-	session_start();
-  if(isset($_POST['login'])){
-    include('includes/connection.php');
-    $query = "select * from users where email = '$_POST[email]' AND password = '$_POST[password]'";
-  	$query_run = mysqli_query($connection,$query);
-    if(mysqli_num_rows($query_run)){
-			$_SESSION['email'] = $_POST['email'];
-			while($row = mysqli_fetch_assoc($query_run)){
-				$_SESSION['fname'] = $row['fname'];
-				$_SESSION['lname'] = $row['lname'];
-				$_SESSION['uid'] = $row['sno'];
-			}
-      echo "<script type='text/javascript'>
-      	window.location.href = 'user_dashboard.php';
-      </script>";
+
     }
     else{
       echo "<script type='text/javascript'>
