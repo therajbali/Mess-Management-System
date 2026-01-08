9eed17b15fb78d5928906c7e76cc90ddc94d13f0
RAJBALI KUMAR <91846008+therajbali@users.noreply.github.com>
2026-01-08 20:57:25 +0530

    Fix query string formatting in login process

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	session_start();
   if(isset($_POST['login'])){
     include('includes/connection.php');
-    $query = "select * from users where email = '$_POST[email]' AND password = '$_POST[password]'";
+    $query = "select * from users where email = '$_POST[email]' AND password = '$_POST[password]'"; 
   	$query_run = mysqli_query($connection,$query);
     if(mysqli_num_rows($query_run)){
 			$_SESSION['email'] = $_POST['email'];
